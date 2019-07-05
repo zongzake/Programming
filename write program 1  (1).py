300496570276b64c78d234ff46b01785588f32d4
zongzake <52312487+zongzake@users.noreply.github.com>
2019-07-05 16:30:34 +0700

    Add files via upload

---
+++ write program 1  (1).py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+print("Pong Shop")
+print("----------------------------------")
+print("1.banana       x 6 = 60")
+print("2.water         x 2 = 30")
+print("3.hamburgur x 1 = 35")
+print("----------------------------------")
+print("total                   = 125")
+
+
+# In[5]:
+
+
+print(10)
+
+
+# In[9]:
+
+
+print(type(10))
+print(type("hello"))
+print(type(True))
+print(type(10.5))
+print(type(1j))
+
+
+# In[11]:
+
+
+print(10+3.5)
+
