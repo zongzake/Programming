e5a09dcb422b7e2e1f9ec0665fddb2f94ea38c67
zongzake <52312487+zongzake@users.noreply.github.com>
2019-08-30 14:02:21 +0700

    Add files via upload

---
+++ Grade.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+x = int(input("score:"))
+if 100>=x>=80:
+    print("A")
+elif 80>x>=75:
+    print("B+")
+elif 75>x>=70:
+    print("B")
+elif 70>x>=65:
+    print("C+")
+elif 65>x>=60:
+    print("C")
+elif 60>x>=55:
+    print("D+")
+elif 55>x>=50:
+    print("D")
+elif 50>x:
+    print("F")
+else:
+    print("error")
+
+
+# In[ ]:
+
+
+
+
---
+++ Temp.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+temperature = float(input("temp ํC :" ))
+temperature_convert_ํC_to_ํF = (9/5*temperature)+32
+temperature_convert_ํC_to_ํK = (temperature + 273.15)
+print("temp ํF = ", temperature_convert_ํC_to_ํF,"      " "temp ํK = " ,temperature_convert_ํC_to_ํK)
+
+
+# In[ ]:
+
+
+
+
